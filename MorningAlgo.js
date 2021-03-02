a8ee1ed2d5375356c025508524489ae6170db4e0
timfoltz <73617242+timfoltz@users.noreply.github.com>
2021-03-01 17:21:51 -0700

    update

--- MorningAlgo.js
+++ MorningAlgo.js
@@ -1,75 +1,122 @@
 
 
-
-// ******************Feb 18 2021*************
-
-// Queue
-// FIFO (First in, first out)
-
-class Queue {
-    constructor() {
-        this.front = null; // sometimes called head "front of the line"
-        this.back = null; // sometimes called rear or tail "back of the line"
-        this.length = 0;
-    }
-
-    // add nodes to the rear of the queue
-    // [d,n] -> [d,n]
-    enqueue(node) {
-        if (!this.front) {
-            this.front = node;
-            this.back = node;
-        } else {
-            this.back.next = node;
-            this.back = node;
-        }
-        this.length++;
-    }
-
-    // remove nodes from the front of the queue
-    dequeue() {
-        if (length === 0) {
-            return -1;
-        } else {
-            this.front = null;
-        }
-    }
-
-    // check the front of the queue without removing it
-    peek() {}
-
-    // return true / false if queue is empty
-    isEmpty() {}
-
-    // return length
-    length() {}
-}
+// ******************Feb 11 2021*************
+var numberOfNodes = 0;
 
 class Node {
     constructor(data) {
         this.data = data;
         this.next = null;
+        numberOfNodes++;
     }
 }
 
-// print every value in the queue
-// you may only use one queue or stack for additional storage
-// return the queue back to it's original order when you are done
-// you are not allowed to linearly traverse the queue
-// only use public methods enqueue, dequeue, peek, isempty, and length
-function readQueue(queue) {}
+class SLL {
 
-var thisQueue = new Queue();
-var node1 = new Node(15);
-var node2 = new Node(18);
-var node3 = new Node(29);
-thisQueue.enqueue(node1);
-thisQueue.enqueue(node2);
-thisQueue.enqueue(node3);
-thisQueue.dequeue();
-console.log(thisQueue);
-// ******************Feb 17 2021*************
+    constructor() {
+        this.head = null;
+    }
 
+
+
+    // ------------------------------------------------------------------------------------------------------
+    //// THURSDAY
+    // ------------------------------------------------------------------------------------------------------
+    // if val is contained within the current list, delete it.
+    // return void
+    // assume there are no duplicates
+    // consider the edge case if you have to delete the head node
+    // consider the edge case your list is empty
+    // consider the edge case that your list does not contain the val
+    // If current.next.value = value, then change the read path
+    delete(data) {}
+
+    // Return the total amount of nodes in the list
+    size() {
+        console.log(`There are ${numberOfNodes} nodes`);
+    }
+
+
+    // ------------------------------------------------------------------------------------------------------
+    //// WEDNESDAY
+    // ------------------------------------------------------------------------------------------------------
+
+    // console log (print) the data of every node in the current list
+    read() {
+        var current = this.head;
+
+        while (current) {
+            console.log("Data of this node: " + current.data);
+            current = current.next;
+        }
+    }
+
+    // find: return true / false if current list contains a data equal to value
+    contains(value) {
+        var runner = this.head;
+
+        while (runner) {
+            if (runner.data == value) {
+                return true;
+            }
+            runner = runner.next
+        }
+        return false;
+    }
+
+    // Remove from front: remove and return the first node in the SLL
+    removeFromFront() {
+        if (this.isEmpty()) return null;
+
+        var removed = this.head;
+        this.head = this.head.next;
+        removed.next = null;
+        return removed;
+    }
+
+    // ------------------------------------------------------------------------------------------------------
+    //// TUESDAY
+    // ------------------------------------------------------------------------------------------------------
+
+    // return true or false if this.head is null
+    isEmpty() {
+        if (this.head) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // add given node to the head, if it exists. return void
+    addToFront(node) {
+        node.next = this.head;
+        this.head = node;
+    }
+
+    // createa  new node with given data, add it to the head. return void
+    addDataToFront(data) {
+        var newNode = new Node(data);
+        newNode.next = this.head
+        this.head = newNode; // move the head to the new node
+    }
+
+
+}
+
+
+var mySLL = new SLL();
+
+mySLL.addToFront(new Node(13));
+mySLL.addToFront(new Node(17));
+mySLL.addToFront(new Node(18));
+mySLL.addToFront(new Node(22));
+mySLL.addDataToFront(27);
+
+mySLL.read();
+console.log(mySLL.contains(13));
+console.log(mySLL.contains(14));
+mySLL.size();
 // ******************Feb 10 2021*************
 // morning algos
 // singly linked list
